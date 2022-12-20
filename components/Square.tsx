f99d478997ec0bb9967b402439ad77b101f10b61
Anas Saoui <saouianas@hotmail.fr>
2022-12-20 22:28:13 +0100

    X or O relays on random event and conditional rendering

---
+++ components/Square.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Square = (props: Props) => {
+  return (
+    <button>This is a square</button>
+  )
+}
+
+export default Square
--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 type Props = {}
 
 const Board = (props: Props) => {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [currentPlayer, setCurrentPlayer] = useState<'X' | 'O'>(
+        Math.round(Math.random()) === 1 ? 'X' : 'O'
+    );
   return (
-    <div>This is the Board</div>
+    <div><p>Hey {currentPlayer} it&apos;s your turn</p></div>
   )
 }
 

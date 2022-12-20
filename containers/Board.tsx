a02e9e6b9e08a851cdbced8e4c5756ce6bdc7181
Anas Saoui <saouianas@hotmail.fr>
2022-12-20 19:30:56 +0100

    added "Board" container component

---
+++ containers/Board.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Board = (props: Props) => {
+  return (
+    <div>This is the Board</div>
+  )
+}
+
+export default Board
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-
+import Board from '../containers/Board'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        Hello
+        <Board />
       </main>
     </>
   )
